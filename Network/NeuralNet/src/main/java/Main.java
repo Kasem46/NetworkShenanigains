4ce1c749ca622fb273f76c60e5e7aa72491a5df1
Kasem46 <gabrielher2678@gmail.com>
2024-04-30 12:52:27 -0400

    Update Main.java

--- Network/NeuralNet/src/main/java/Main.java
+++ Network/NeuralNet/src/main/java/Main.java
@@ -13,7 +13,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic 
+        System.out.println();
     }
     
 }
